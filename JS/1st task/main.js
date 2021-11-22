58ddd58cdf73332cfa236ce6869bedd7c728758b
unknown <upmewakelvmpls@gmail.com>
2021-11-22 17:28:16 +0300

    react task

--- JS/1st task/main.js
+++
@@ -1,5 +0,0 @@
-function firstLetterToUpperCase(word) {
-    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-}
-
-console.log(firstLetterToUpperCase('javaSCRIPT')); // Javascript
--- JS/2nd task/main.js
+++
@@ -1,7 +0,0 @@
-function sayHello(name) {
-    if (name === 'Mark' || name === 'mark') return 'Hi, ' + name + '!'
-    else return 'Hello, ' + name + '!';
-}
-
-console.log(sayHello('Mark'));
-console.log(sayHello('Oleg'));
