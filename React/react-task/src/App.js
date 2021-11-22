0a64c13ebbaf0598c7bf1adc1ba0a7083faaf9af
unknown <upmewakelvmpls@gmail.com>
2021-11-22 18:48:20 +0300

    react task

--- React/react-task/src/App.js
+++ React/react-task/src/App.js
@@ -10,15 +10,34 @@ function App() {
       setCount(count - 1);
     } 
   }
-
+  
   function increment() {
-      setCount(count + 1); 
+    setCount(count + 1); 
+    
   }
+  
+  let getBackgroundColor = () => {
+    let color ='red';
+    if (count % 2 === 0) {
+      color = 'red';
+    } else {
+      color = 'blue';
+    }
+    return color;
+};
 
   return (
-    <div className="App">
-      <div className="current-number">{count}</div>
-      <div>{ count % 2 == 0 ? 'Введено четное число' : 'Введено нечетное число' }</div>
+    <div className="App"
+         style={{
+                  backgroundColor: getBackgroundColor()
+               }}
+    >
+      <div className="current-number" 
+      >{count}</div>
+      <div>
+        { count % 2 === 0 ? 'Введено четное число' : 'Введено нечетное число' }
+      </div>
+      
       <div>
         <button onClick={decrement}>-</button>
         <button onClick={() => setCount(0)}>Reset</button>
